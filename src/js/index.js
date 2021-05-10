2d38048a0c397652d383c77a68e6723da0b488d4
FrederickLen <71602899+FrederickLen@users.noreply.github.com>
2021-05-09 21:02:44 -0400

    What

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,21 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "YOURKEYHERE";
+//
+//
+const query = async function () {
+  try {
+    const response = await fetch(
+      "https://botw-compendium.herokuapp.com/api/v2"
+    );
+    const data = await response.json();
+    data.results.forEach((movie) => {
+      DOMSelectors.grid.insertAdjacentHTML("beforeend", "");
+    });
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong");
+  }
+};
+query();
