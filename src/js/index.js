4d6fa7b7c5543f56d7e6afeef48b5c0db19f8027
FrederickLen <71602899+FrederickLen@users.noreply.github.com>
2021-05-24 17:44:21 -0400

    Update index.js
    Adding in all the categories

--- src/js/index.js
+++ src/js/index.js
@@ -10,12 +10,191 @@ const query = async function () {
     resData.data.monsters.forEach((element) => {
       let categoryArr = [];
       const addCategory = function () {
-        //   botw.forEach((element) => {
-        //     if (element.name.includes(element.name)) {
-        //       categoryArr.push(element.name);
-        //       return categoryArr;
-        //     }
-        //   });
+        categoryArr.push(element.name);
+        return categoryArr;
+      };
+      addCategory();
+      console.log(categoryArr);
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `</div><div class="element-card">
+        <div class="element-card-front">
+          <img
+            src="https://botw-compendium.herokuapp.com/api/v2/entry/${element.id}/image"
+            alt=""
+            class="image"
+          />
+        </div>
+        <div class="element-card-back">
+          <h3 class="element-card-header">${element.name}</h3>
+          <div class="location-box">
+          <p class="common-locations">Common Locations</p>
+            <p class="common-locations">${element.common_locations}</p>
+          </div>
+
+          <div class="drops-box">
+          <p class="item-drops">Item Drops</p>
+            <p class="item-drops">${element.drops}</p>
+          </div>
+
+          <div class="categories">
+          <li class="category">${categoryArr}</li>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+    resData.data.equipment.forEach((element) => {
+      let categoryArr = [];
+      const addCategory = function () {
+        categoryArr.push(element.name);
+        return categoryArr;
+      };
+      addCategory();
+      console.log(categoryArr);
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `</div><div class="element-card">
+        <div class="element-card-front">
+          <img
+            src="https://botw-compendium.herokuapp.com/api/v2/entry/${element.id}/image"
+            alt=""
+            class="image"
+          />
+        </div>
+        <div class="element-card-back">
+          <h3 class="element-card-header">${element.name}</h3>
+          <div class="location-box">
+          <p class="common-locations">Common Locations</p>
+            <p class="common-locations">${element.common_locations}</p>
+          </div>
+
+          <div class="drops-box">
+          <p class="item-drops">Item Drops</p>
+            <p class="item-drops">${element.drops}</p>
+          </div>
+
+          <div class="categories">
+          <li class="category">${categoryArr}</li>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+    resData.data.materials.forEach((element) => {
+      let categoryArr = [];
+      const addCategory = function () {
+        categoryArr.push(element.name);
+        return categoryArr;
+      };
+      addCategory();
+      console.log(categoryArr);
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `</div><div class="element-card">
+        <div class="element-card-front">
+          <img
+            src="https://botw-compendium.herokuapp.com/api/v2/entry/${element.id}/image"
+            alt=""
+            class="image"
+          />
+        </div>
+        <div class="element-card-back">
+          <h3 class="element-card-header">${element.name}</h3>
+          <div class="location-box">
+          <p class="common-locations">Common Locations</p>
+            <p class="common-locations">${element.common_locations}</p>
+          </div>
+
+          <div class="drops-box">
+          <p class="item-drops">Item Drops</p>
+            <p class="item-drops">${element.drops}</p>
+          </div>
+
+          <div class="categories">
+          <li class="category">${categoryArr}</li>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+    resData.data.creatures.food.forEach((element) => {
+      let categoryArr = [];
+      const addCategory = function () {
+        categoryArr.push(element.name);
+        return categoryArr;
+      };
+      addCategory();
+      console.log(categoryArr);
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `</div><div class="element-card">
+        <div class="element-card-front">
+          <img
+            src="https://botw-compendium.herokuapp.com/api/v2/entry/${element.id}/image"
+            alt=""
+            class="image"
+          />
+        </div>
+        <div class="element-card-back">
+          <h3 class="element-card-header">${element.name}</h3>
+          <div class="location-box">
+          <p class="common-locations">Common Locations</p>
+            <p class="common-locations">${element.common_locations}</p>
+          </div>
+
+          <div class="drops-box">
+          <p class="item-drops">Item Drops</p>
+            <p class="item-drops">${element.drops}</p>
+          </div>
+
+          <div class="categories">
+          <li class="category">${categoryArr}</li>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+    resData.data.creatures.non_food.forEach((element) => {
+      let categoryArr = [];
+      const addCategory = function () {
+        categoryArr.push(element.name);
+        return categoryArr;
+      };
+      addCategory();
+      console.log(categoryArr);
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `</div><div class="element-card">
+        <div class="element-card-front">
+          <img
+            src="https://botw-compendium.herokuapp.com/api/v2/entry/${element.id}/image"
+            alt=""
+            class="image"
+          />
+        </div>
+        <div class="element-card-back">
+          <h3 class="element-card-header">${element.name}</h3>
+          <div class="location-box">
+          <p class="common-locations">Common Locations</p>
+            <p class="common-locations">${element.common_locations}</p>
+          </div>
+
+          <div class="drops-box">
+          <p class="item-drops">Item Drops</p>
+            <p class="item-drops">${element.drops}</p>
+          </div>
+
+          <div class="categories">
+          <li class="category">${categoryArr}</li>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+    resData.data.treasure.forEach((element) => {
+      let categoryArr = [];
+      const addCategory = function () {
         categoryArr.push(element.name);
         return categoryArr;
       };
