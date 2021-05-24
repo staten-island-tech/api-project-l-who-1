b9ae20f3a0032de1f6115a6ad9ae1c4ae3809b22
FrederickLen <71602899+FrederickLen@users.noreply.github.com>
2021-05-23 21:01:57 -0400

    Finally got the categories working! (Kinda)

--- src/js/index.js
+++ src/js/index.js
@@ -6,31 +6,33 @@ const query = async function () {
     const response = await fetch(
       `https://botw-compendium.herokuapp.com/api/v2`
     );
-    const data = await response.json();
-    data.response.forEach((element) => {
+    const resData = await response.json();
+    resData.data.monsters.forEach((element) => {
       let categoryArr = [];
       const addCategory = function () {
-        botw.forEach((element) => {
-          if (element.names.includes(element.name)) {
-            categoryArr.push(element.name);
-            return categoryArr;
-          }
-        });
+        //   botw.forEach((element) => {
+        //     if (element.name.includes(element.name)) {
+        //       categoryArr.push(element.name);
+        //       return categoryArr;
+        //     }
+        //   });
+        categoryArr.push(element.name);
+        return categoryArr;
       };
       addCategory();
-      console.log(genereArr);
+      console.log(categoryArr);
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
         `</div><div class="element-card">
         <div class="element-card-front">
           <img
-            src="https://botw-compendium.herokuapp.com/api/v2/entry/${element.image}/image"
+            src="https://botw-compendium.herokuapp.com/api/v2/entry/${element.id}/image"
             alt=""
             class="image"
           />
         </div>
         <div class="element-card-back">
-          <h3 class="element-card-header">Horse</h3>
+          <h3 class="element-card-header">${element.name}</h3>
           <div class="location-box">
           <p class="common-locations">Common Locations</p>
             <p class="common-locations">${element.common_locations}</p>
@@ -48,7 +50,7 @@ const query = async function () {
       </div>`
       );
     });
-    console.log(data);
+    console.log(resData);
   } catch (error) {
     console.log(error);
     alert("Something went wrong");
